57e98675f9870846b4546ea0c7a178fc20872a3f
Mohamed <mansourmohamedsimplonenda@gmail.com>
2021-04-14 10:46:04 +0100

    local storage

---
+++ js/badge.js
@@ -0,0 +1,19 @@
+var compt = document.querySelectorAll('#btn')
+console.log(compt[0])
+for(let i=0;i<compt.length;i++){
+    compt[i].addEventListener("click", () => {
+        kl()
+    })
+}
+function kl(){
+    let local=localStorage.getItem("kl")
+    local=parseInt(local);
+    if (local){
+      localStorage.setItem("kl",local+1)
+      document.getElementById('val').innerHTML=local+1
+    }
+    else{
+        localStorage.setItem("kl",1)
+        document.getElementById('val').innerHTML=1
+    }
+}
